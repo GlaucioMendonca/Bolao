639a319a2306f071a886b7c5a8c4f97237bde5c5
ifpb <verribeiro@hotmail.com>
2018-07-19 17:03:27 -0300

    add view bolao

---
+++ Appbolao/app/src/main/java/com/example/glaucio/appbolao/CadastroBolao.kt
@@ -0,0 +1,18 @@
+package com.example.glaucio.appbolao
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_cadastro_bolao.*
+
+class CadastroBolao : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_bolao)
+        setSupportActionBar(toolbar)
+
+    }
+
+}
---
+++ Appbolao/app/src/main/java/com/example/glaucio/appbolao/ViewBolao.kt
@@ -0,0 +1,12 @@
+package com.example.glaucio.appbolao
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewBolao : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_bolao)
+    }
+}
