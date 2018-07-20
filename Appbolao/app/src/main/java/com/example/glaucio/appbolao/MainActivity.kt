9590b6de71ac50acbb93e8963948081fb642367d
GlaucioMendonca <glaucio43@hotmail.com>
2018-07-19 21:18:46 -0300

    Add view de cadastro de bolao
    criação da view de cadastro

--- Appbolao/app/src/main/java/com/example/glaucio/appbolao/MainActivity.kt
+++ Appbolao/app/src/main/java/com/example/glaucio/appbolao/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bolao: Bolao
     private lateinit var lv: ListView
     private lateinit var apostadordao: ApostadorDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
